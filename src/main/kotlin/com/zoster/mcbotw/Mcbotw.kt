3f3a25f745bb61ca68cc0bddf74d80c8e8b9d845
ZosTer17 <abdulilprincipalekebabbaro.121@gmail.com>
2025-09-07 19:11:10 +0200

    Aggiunto l'evento PlayerJoin (lato server)

--- src/main/kotlin/com/zoster/mcbotw/Mcbotw.kt
+++ src/main/kotlin/com/zoster/mcbotw/Mcbotw.kt
@@ -1,14 +1,21 @@
 package com.zoster.mcbotw
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import org.slf4j.LoggerFactory
 
-class Mcbotw : ModInitializer {
-    companion object {
-        const val MOD_ID = "mcbotw"
-        val logger = LoggerFactory.getLogger(MOD_ID)
-    }
+object Mcbotw : ModInitializer {
+    private const val MOD_ID = "mcbotw"
+    private val logger = LoggerFactory.getLogger(MOD_ID)
 
     override fun onInitialize() {
+        logger.info("La mod è stata attivata!")
+
+        ServerPlayConnectionEvents.JOIN.register { handler, _, _ ->
+            val player = handler.player
+            player.inventory.insertStack(0, ItemStack(Items.BLUE_ICE))
+        }
     }
 }
